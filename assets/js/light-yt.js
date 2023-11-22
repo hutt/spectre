bee4a9b6b2a6737ddeac10f5f14da058a7865c02
hutt <jannis@hutt.io>
2023-11-22 07:58:12 +0100

    optimized light-yt.js's image previews (jpeg/webp; 300w, 600w, 1000w)

--- assets/js/light-yt.js
+++ assets/js/light-yt.js
@@ -40,10 +40,10 @@ function initYouTubeVideos() {
     div.setAttribute('data-id', videoId);
     let picture = div.appendChild(document.createElement('picture'));
     let placeholder_webp = picture.appendChild(document.createElement('source'));
-    placeholder_webp.srcset = '//i.ytimg.com/vi_webp/ID/hqdefault.webp'.replace('ID', videoId);
+    placeholder_webp.srcset = '//i.ytimg.com/vi_webp/ID/mqdefault.webp 300w, //i.ytimg.com/vi_webp/ID/hqdefault.webp 600w, //i.ytimg.com/vi_webp/ID/maxresdefault.webp 1000w'.replaceAll('ID', videoId);
     placeholder_webp.type = 'image/webp';
     let placeholder_jpg = picture.appendChild(document.createElement('source'));
-    placeholder_jpg.srcset = '//i.ytimg.com/vi/ID/hqdefault.jpg'.replace('ID', videoId);
+    placeholder_jpg.srcset = '//i.ytimg.com/vi/ID/mqdefault.jpg 300w, //i.ytimg.com/vi/ID/hqdefault.jpg 600w, //i.ytimg.com/vi/ID/maxresdefault.jpg 1000w'.replaceAll('ID', videoId);
     placeholder_jpg.type = 'image/jpeg';
     let placeholder = picture.appendChild(document.createElement('img'));
     placeholder.src = '//i.ytimg.com/vi/ID/hqdefault.jpg'.replace('ID', videoId);
